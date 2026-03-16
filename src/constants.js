206a996969a2bbc8729cd84bf248b7f5af7f1f88
filippernar <filippernar@gmail.com>
2026-03-16 17:44:43 +0100

    rad na aplikaciji 1

---
+++ src/constants.js
@@ -0,0 +1,6 @@
+export const IME_APLIKACIJE='Evidencija servisnih usluga automehaničarske radionice'
+
+export const RouteNames = {
+    HOME: '/',
+    USLUGE: '/usluge' 
+}
---
+++ src/pages/usluge/UslugePregled.jsx
@@ -0,0 +1,7 @@
+export default function UslugePregled(){
+    return(
+        <>
+        Ovdje će se vidjeti usluge
+        </>
+    )
+}
