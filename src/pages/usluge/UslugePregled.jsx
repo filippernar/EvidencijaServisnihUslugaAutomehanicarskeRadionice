a096fcd77d83d0508043e4087e9124bc276377c7
filippernar <filippernar@gmail.com>
2026-03-19 17:59:19 +0100

    tablica

--- src/pages/usluge/UslugePregled.jsx
+++ src/pages/usluge/UslugePregled.jsx
@@ -1,5 +1,9 @@
 import { useEffect, useState } from "react"
 import UslugeService from "../../services/usluge/UslugeService"
+import { Table } from "react-bootstrap"
+import { NumericFormat } from "react-number-format"
+import { GrValidate } from "react-icons/gr"
+import FormatDatuma from "../../components/FormatDatuma"
 
 export default function UslugePregled(){
 
@@ -20,11 +24,48 @@ export default function UslugePregled(){
 
     return(
         <>
-        <ul>
-            {usluge && usluge.map((usluge)=>(
-                <li>{usluge.naziv}</li>
-            ))}
-        </ul>
+            <Table>
+                <thead>
+                    <tr>
+                        <th>Naziv</th>
+                        <th>Trajanje</th>
+                        <th>Cijena</th>
+                        <th>Datum pokretanja</th>
+                        <th>Aktivan</th>
+                        <th>Akcija</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {usluge && usluge.map((usluge)=>(
+                        <tr>     
+                            <td>{usluge.naziv}</td>
+                            <td>{usluge.trajanje}</td>
+                            <td>
+                                <NumericFormat 
+                                value={usluge.cijena}
+                                displayType={'text'}
+                                thousandSeparator='.'
+                                decimalSeparator=','
+                                suffix={' €'}
+                                decimalScale={2}
+                                fixedDecimalScale
+                                />
+                            </td>
+                            <td>
+                                <FormatDatuma datum={usluge.datumPokretanja} />
+
+                            </td>
+                            <td>
+                                <GrValidate 
+                                size={25}
+                                color={usluge.aktivan ? 'green' : 'red'}
+                                />
+                            </td>
+                            <td></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
         </>
     )
 }
