4d0e130dbe01136b4a2870fb5e6fbb46126ed72b
filippernar <filippernar@gmail.com>
2026-04-28 19:09:56 +0200

    Straničenje vozila

--- src/pages/vozilo/VoziloPregled.jsx
+++ src/pages/vozilo/VoziloPregled.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 import VoziloService from "../../services/vozilo/VoziloService"
-import { Button, Table } from "react-bootstrap"
+import { Button, Table, Pagination } from "react-bootstrap"
 import { Link, useNavigate } from "react-router-dom"
 import { RouteNames } from "../../constants"
 
@@ -9,6 +9,10 @@ export default function VoziloPregled(){
     const navigate = useNavigate()
 
     const [vozila, setVozila] = useState([])
+
+    // Straničenje
+    const [currentPage, setCurrentPage] = useState(1)
+    const pageSize = 5  // broj vozila po stranici
 
     useEffect(()=>{
         ucitajVozila()
@@ -32,12 +36,26 @@ export default function VoziloPregled(){
         })
     }
 
+    // Izračun stranica
+    const totalPages = Math.ceil(vozila.length / pageSize)
+
+    const prikazanaVozila = vozila.slice(
+        (currentPage - 1) * pageSize,
+        currentPage * pageSize
+    )
+
+    function promijeniStranicu(page) {
+        if (page < 1 || page > totalPages) return
+        setCurrentPage(page)
+    }
+
     return(
         <>
         <Link to={RouteNames.VOZILA_NOVI}
         className="btn btn-success w-100 my-3">
             Dodavanje novog vozila
         </Link>
+
         <Table striped bordered hover responsive>
             <thead>
                 <tr>
@@ -50,7 +68,7 @@ export default function VoziloPregled(){
                 </tr>
             </thead>
             <tbody>
-                {vozila && vozila.map((vozilo)=>(
+                {prikazanaVozila.map((vozilo)=>(
                     <tr key={vozilo.sifra}>
                         <td>{vozilo.registracija}</td>
                         <td>{vozilo.marka}</td>
@@ -70,6 +88,24 @@ export default function VoziloPregled(){
                 ))}
             </tbody>
         </Table>
+
+        {/*  Bootstrap Pagination */}
+        <Pagination className="justify-content-center">
+            <Pagination.Prev onClick={() => promijeniStranicu(currentPage - 1)} disabled={currentPage === 1} />
+
+            {[...Array(totalPages)].map((_, i) => (
+                <Pagination.Item
+                    key={i}
+                    active={i + 1 === currentPage}
+                    onClick={() => promijeniStranicu(i + 1)}
+                >
+                    {i + 1}
+                </Pagination.Item>
+            ))}
+
+            <Pagination.Next onClick={() => promijeniStranicu(currentPage + 1)} disabled={currentPage === totalPages} />
+        </Pagination>
+
         </>
     )
 }
